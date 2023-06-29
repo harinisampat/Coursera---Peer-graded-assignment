ad369e519f9a50335d085a6e5ebb7b524b849251
harinisampat <124083498+harinisampat@users.noreply.github.com>
2023-06-29 13:54:28 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -140,7 +140,7 @@ app.layout = html.Div(children=[
                 Output(component_id='plot2', component_property='children'),
                 Output(component_id='plot3', component_property='children'),
                 Output(component_id='plot4', component_property='children'),
-                Output(component_id='plot5', component_property='children'),
+                Output(component_id='plot5', component_property='children')
                 ],
                [Input(component_id='input-type', component_property='value'),
                 Input(component_id='input-year', component_property='value')],
